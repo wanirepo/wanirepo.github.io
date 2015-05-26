a84b4337cbbdf2cd6d56e59f9221b5a71b9e2c2d
wanirepo <clinpsywoo@gmail.com>
2015-05-25 23:42:14 -0600

    add a network to the repository

---
+++ pictures/pub_network.m
@@ -0,0 +1,215 @@
+A = [0	0	0	0	1	1	0	0	1	1	0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	1	1	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	1	1	0	0	0	1	0	1	1	1	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+1	0	1	0	0	0	0	0	0	0	0	0	0	0	0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	1
+1	0	1	0	0	0	0	0	0	0	0	0	0	0	0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	1
+0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	1	0	0	1	1	1	1	0	0	0	0	0	0	0	0	0	0	0	0	0	1
+0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	1	0	0	0	0	0	0	1	1	1	0	0	0	0	0	0	0	0	0	0	1
+1	1	0	0	0	0	0	0	0	0	0	0	0	0	0	1	1	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	1
+1	0	1	0	0	0	0	0	0	0	0	0	0	0	0	1	0	0	0	0	0	0	0	0	0	1	1	1	1	1	1	0	0	0	0	1
+0	0	0	1	0	0	0	0	0	0	0	0	0	0	0	1	0	0	0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	1
+1	0	1	0	0	0	0	0	0	0	0	0	0	0	0	1	1	0	0	0	0	0	0	0	0	0	1	1	0	0	0	1	0	0	0	1
+0	0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	1	1	1	1
+0	0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	1	1
+0	0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	1	0	1	1
+0	0	0	0	1	1	1	1	1	1	1	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	1	0	0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	1	0	0	0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	1	0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	1	0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	0	0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	0	0	0	1	0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	0	0	0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	0	0	0	1	1	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	1	1	1	1	1	1	1	1	1	1	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0];
+
+node_name = {'FMRI-based biomarker',...
+'Pain/emotion regulation',...
+'Translation',...
+'FMRI methods',...
+'2015 PAIN',...
+'2015 STM',...
+'2015 Cult Brain',...
+'2015 Drug',...
+'2015 PLoS Biol',...
+'2014 Nat Comms',...
+'2014 Neuroimage',...
+'2013 NEJM',...
+'2010 JBTEP',...
+'2010 KCP',...
+'2010 KCP',...
+'Wager',...
+'Roy',...
+'Buhle',...
+'Losin',...
+'Krishnan',...
+'Iacoboni',...
+'Dapretto',...
+'Yamamoto',...
+'Regner',...
+'Tanabe',...
+'Koban',...
+'Kross',...
+'Lindquist',...
+'Banich',...
+'Ruzic',...
+'Andrews-Hanna',...
+'Atlas',...
+'Kwon',...
+'Lim',...
+'Shin',...
+'Woo'};
+
+groups{1} = [0
+0
+0
+0
+1
+2
+2
+2
+1
+1
+1
+2
+1
+1
+1
+3
+3
+3
+3
+3
+3
+3
+3
+3
+3
+3
+3
+3
+3
+3
+3
+3
+3
+3
+3
+4];
+
+values = [15
+15
+15
+15
+5
+5
+4
+4
+5
+14
+39
+153
+18
+4
+4
+4
+4
+4
+4
+4
+4
+4
+4
+4
+4
+4
+4
+4
+4
+4
+4
+4
+4
+4
+4
+4];
+
+visualize_network(A, 'node_names', node_name, 'groups', groups, 'values', values)
+A(36,:) =[];
+A(:,36) =[];
+groups{1}(36) = [];
+values(36) = [];
+node_name(36) =[];
+
+
+%%
+create_figure('network')
+sA = sparse(A);
+X = fruchterman_reingold_force_directed_layout(sA);
+% X = fruchterman_reingold_force_directed_layout(sA, 'initial_temp',1.4);
+% X = kamada_kawai_spring_layout(sA, 'spring_constant', 5);
+X(12,1)=X(12,1)-1.5;
+
+col = [0.5843    0.2157    0.2078
+    0.4667    0.5765    0.2353
+    0.2157    0.3765    0.5725
+    .4 .4 .4
+    .6 0 0]*1.4;
+col(col>1) = 1;
+
+col2 = [0.5843    0.2157    0.2078
+    0.4667    0.5765    0.2353
+    0.2157    0.3765    0.5725
+    Inf Inf Inf
+    1 0 0]*.8;
+col2(col2>1) = 1;    
+
+linewidths = [1.5 1.5 1.5 2];
+
+[i,j,x] = find(sA);
+
+for k = 1:numel(i)
+    hl(k) = line([X(i(k),1) X(j(k),1)], [X(i(k),2) X(j(k),2)]);
+%     set(hl(k), 'color', [.7 .7 .7], 'linewidth', .5);
+    set(hl(k), 'color', [1 1 1], 'linewidth', .5);
+    %set(hl(k), 'color', [.3 .3 .3], 'linewidth', .5);
+    hold on;
+end
+
+for k = 1:size(X,1)
+%     if groups{1}(k)==0
+        h(i) = scatter(X(k,1), X(k,2), values(k)*30, 'filled', 'MarkerFaceColor', col(groups{1}(k)+1,:), 'MarkerEdgeColor', col2(groups{1}(k)+1,:), 'LineWidth', linewidths(groups{1}(k)+1));
+        %     h(i) = scatter(X(k,1), X(k,2), 50, 'filled', 'MarkerFaceColor', col(groups{1}(k)+1,:), 'MarkerEdgeColor', col2(groups{1}(k)+1,:), 'LineWidth', 1);
+        hold on;
+%     end
+end
+
+axis off;
+set(gcf, 'position', [244   267   484   439]);
+
+%%
+
+figdir = '/Users/clinpsywoo/github/wanirepo.github.io/pictures';
+% savename = fullfile(figdir, 'pub_network');
+savename = fullfile(figdir, 'pub_network3');
+% savename = fullfile(figdir, 'pub_network2');
+
+try
+    pagesetup(gcf);
+    saveas(gcf, savename, 'pdf');
+catch
+    pagesetup(gcf);
+    saveas(gcf, savename, 'pdf');
+end
